540b3f99f08161065df8b425a2a85abb1be9b032
joshua kim <j0shxone@gmail.com>
2020-10-23 10:38:47 -0400

    First commit.

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+// register and login routes
+
+app.use("/auth", require("./routes/jwtAuth"));
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+});
